2d90efebbff2340f21d9d9d18d4e26855bd53f56
Andrey Marinov <heavenlymage97@gmail.com>
2022-08-29 09:54:04 +0300

    Testing my_math

--- test_my_math.py
+++ test_my_math.py
@@ -1,40 +1,42 @@
 '''Testing MyMath module for proper performing on four methods - Add,Sub,Multi,Divide '''
 import unittest
+
+
 from my_math import MyMath
 
 
 class TestMath(unittest.TestCase):
     '''Starting the tests with unittest'''
 
-    def test_addition(self):
-        '''Testing the addition method'''
-        self.assertEqual(MyMath.addition(self, 15, 15), 30)
-        self.assertEqual(MyMath.addition(self, 30, 15), 45)
-        self.assertEqual(MyMath.addition(self, 151, 15), 166)
-        self.assertEqual(MyMath.addition(self, -34, 15), -19)
+    def test_addition_of_two_positive_integers(self):
+        self.assertEqual(MyMath.addition(2, 2), 4)
 
-    def test_substraction(self):
-        '''Testing the substraction method'''
-        self.assertEqual(MyMath.substraction(self, 45, 15), 30)
-        self.assertEqual(MyMath.substraction(self, 15, 30), -15)
-        self.assertEqual(MyMath.substraction(self, 45, 45), 0)
-        self.assertEqual(MyMath.substraction(self, 45, 35), 10)
+    def test_addition_of_positive_and_negative_integers(self):
+        self.assertEqual(MyMath.addition(2, -2), 0)
 
+    def test_addition_of_two_negative_integers(self):
+        self.assertEqual(MyMath.addition(-2, -2), -4)
 
-    def test_multiplication(self):
-        '''Testing the multiplication method'''
-        self.assertEqual(MyMath.multiplication(self, 8, 8), 64)
-        self.assertEqual(MyMath.multiplication(self, 0, 0), 0)
-        self.assertEqual(MyMath.multiplication(self, -10, 8), -80)
-        self.assertEqual(MyMath.multiplication(self, 5, 8), 40)
+    def test_addition_of_two_negative_and_positive_integers(self):
+        self.assertEqual(MyMath.addition(-2, 2), 0)
+
+    def test_addition_of_two_floats(self):
+        self.assertEqual(MyMath.addition(0.2, 0.1), 0.3)
 
 
-    def test_division(self):
-        '''Testing the divide method'''
-        self.assertEqual(MyMath.division(self, 8, 8), 1)
-        self.assertEqual(MyMath.division(self, 5, 8), 0.625)
-        self.assertEqual(MyMath.division(self, 80, 8),10)
-        self.assertEqual(MyMath.division(self, -40, 8), -5)
+  # def test_substraction(self):
+
+
+
+   # def test_multiplication(self):
+
+
+
+    #def test_division(self):
+
+
+    #def test_divide_by_zero(self):'''
+
 
 if __name__ == '__main__':
     unittest.main()
