bf860f04fa14ad1ab6fbb9b14deabfdb91749234
Andrey Marinov <heavenlymage97@gmail.com>
2022-08-29 09:53:00 +0300

    Program that perform four methods - Addition,Substraction,Multiplication and Division

--- my_math.py
+++ my_math.py
@@ -1,23 +1,34 @@
 '''Program for performing four methods - Addition, Substraction, Multiplication and Division'''
+
+
+class DivideByZeroError(Exception):
+    '''DividingByZero'''
+
+
 class MyMath():
     '''Define my class name as MyMath'''
 
-    def addition(self, number_1, number_2):
+    @staticmethod
+    def addition(number_1, number_2):
         '''Performing the addition method between two numbers'''
         return number_1 + number_2
 
-    def substraction(self, number_1, number_2):
+    @staticmethod
+    def substraction(number_1, number_2):
         '''Performing the substraction method between two numbers'''
         return number_1 - number_2
 
-    def multiplication(self, number_1, number_2):
+    @staticmethod
+    def multiplication(number_1, number_2):
         '''Performing the multiplication method between two numbers'''
         return number_1 * number_2
 
-    def division(self, number_1, number_2):
+    @staticmethod
+    def division(number_1, number_2):
         '''Performing the division method between two numbers'''
+        if number_2 == 0:
+            raise DivideByZeroError("Zero can't be divided by zero")
         return number_1 / number_2
 
-obj=MyMath()
-'''Printing one of the methods on the terminal'''
-print(obj.addition(30, 40))
+obj= MyMath()
+print(obj.addition(9, 0))
